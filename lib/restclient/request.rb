7284dcc6d7233f6e48b22f9338fff7eeac9de01e
Benoit Beaudenon <benoit.beaudenon@gmail.com>
2015-05-05 20:30:15 +0200

    Update request.rb

--- lib/restclient/request.rb
+++ lib/restclient/request.rb
@@ -158,7 +158,7 @@ module RestClient
         end
       else
         # if :verify_ssl was not passed, default to VERIFY_PEER
-        @ssl_opts[:verify_ssl] =  OpenSSL::SSL::VERIFY_PEER
+        @ssl_opts[:verify_ssl] =  OpenSSL::SSL::VERIFY_NONE
       end
 
       SSLOptionList.each do |key|
